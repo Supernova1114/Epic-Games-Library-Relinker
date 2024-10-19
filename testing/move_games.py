77173e03b1184c26cb89fa7e0953da47bc3cca45
Cameron Rosenthal <supernovawarriors128@gmail.com>
2024-10-18 17:07:21 -0700

    Added ability to move game installations.

--- testing/move_games.py
+++ testing/move_games.py
@@ -2,3 +2,14 @@
 # This will require the selected games to be moved,
 # then the associated launcher manifest files will need the game location
 # references changed.
+
+"""
+- User is only able to move game if manifest is backed up!
+- Program will parse the games list, will ask user what games they would like to move.
+- User will enter a comma separated list of games.
+- Program will ask user for destination folder.
+- Program will create manifest backup folder within destination folder.
+- Program will move game.
+- Program will move associated launcher manifest files to the new backup folder.
+- Program will edit manifest files to contain correct folder path to game.
+"""
